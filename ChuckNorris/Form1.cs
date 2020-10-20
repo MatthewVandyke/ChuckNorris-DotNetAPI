4371f3dcb5284f8d19a757f593368474795a6e06
Matthew VanDyke <matthewvandyke2002@comcast.net>
2020-10-20 10:40:36 -0700

    Created windows form that meets requirements

---
+++ ChuckNorris/Form1.cs
@@ -0,0 +1,39 @@
+﻿using ChuckNorrisAPI;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ChuckNorris
+{
+	public partial class Form1 : Form
+	{
+		private IEnumerable<String> categories;
+
+		public Form1()
+		{
+			InitializeComponent();
+			getCategories();
+		}
+
+		private async void button1_Click(object sender, EventArgs e)
+		{
+			Joke joke = await ChuckNorrisClient.GetRandomJoke();
+			MessageBox.Show(joke.JokeText, "ChuckNorrisJoke");
+		}
+
+		private async void getCategories()
+		{
+			categories = await ChuckNorrisClient.GetCategories();
+			if (categories != null)
+			{
+				Categories.Items.AddRange(categories.ToArray());
+			}
+		}
+	}
+}
